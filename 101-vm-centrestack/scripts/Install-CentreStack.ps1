a9b61acf41a77fe43743d3b377a6b3d5d320bb5e
Jeff Reed <jeffr@gladinet.com>
2018-11-14 15:30:35 -0500

    Added processing of Azure Key Vault secrets

--- 101-vm-centrestack/scripts/Install-CentreStack.ps1
+++ 101-vm-centrestack/scripts/Install-CentreStack.ps1
@@ -8,6 +8,8 @@
     .\Install-CentreStack -Build 6033 -Modules @(@{name = 'AzureRM.Compute'; version = '5.8.0'}, @{name = 'AzureRM.Profile'; version = '5.8.0'}, @{name = 'AzureAD'; version = '2.0.2.4'})
 .PARAMETER Build
     The build number to download and install
+.PARAMETER VaultName
+    The name of the Azure Key Vault containing secrets for this script.
 .PARAMETER Modules
     An array of hashtables specifying the name and version of PowerShell modules that will be installed
 .NOTES 
@@ -42,6 +44,15 @@ Param
     [String]
     $Build,
 
+    [Parameter(
+        Mandatory=$true,
+        ValueFromPipelineByPropertyName=$false,
+        HelpMessage="The name of the Azure Key Vault containing secrets for this script."
+    )]
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $VaultName
+
 	[Parameter(
         Mandatory=$false,
         ValueFromPipelineByPropertyName=$false
@@ -56,16 +67,98 @@ function Disable-InternetExplorerESC {
     $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
     Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
     Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-    Stop-Process -Name Explorer
-    Write-Verbose "IE Enhanced Security Configuration (ESC) has been disabled." 
+    Out-Log -Level Info -Message ("IE Enhanced Security Configuration (ESC) has been disabled." )
 } #end function Disable-InternetExplorerESC
+
+function Out-Log {
+    <#  
+    .SYNOPSIS
+        Writes output to the log file.
+    .DESCRIPTION
+        Writes output the Host and appends output to the log file with date/timestamp
+    .PARAMETER Message
+        The string that will be output to the log file
+    .PARAMETER Level
+        One of: "Info", "Warn", "Error", "Verbose"
+    .NOTES    
+        Requires that the $Script:log variable be set by the caller
+    .EXAMPLE
+        Out-Log "Test to write to log"
+    .EXAMPLE
+        Out-Log -Message "Test to write to log" -Level "Info"
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter (
+            Position=0, 
+            Mandatory=$true,
+            ValueFromPipeline=$False,
+            ValueFromPipelineByPropertyName=$False
+        ) ]
+        [string] $Message,
+        [Parameter (
+            Position=1, 
+            Mandatory=$false,
+            ValueFromPipeline=$False,
+            ValueFromPipelineByPropertyName=$False
+        ) ]
+        [ValidateSet("Info", "Warn", "Error", "Verbose")]
+        [string] $Level = "Info"
+    )
+	
+    $ts = $(Get-Date -format "s")
+    $s = ("{0}`t{1}`t{2}" -f $ts, $Level, $Message)
+    if ($Level -eq "Verbose") {
+        # Only log and output if script is called with -Verbose common parameter
+        if ( $VerbosePreference -ne [System.Management.Automation.ActionPreference]::SilentlyContinue ) {
+            Write-Output $s
+            Write-Output $s | Out-File -FilePath $script:log -Encoding utf8 -Append
+        }
+    } 
+    else {
+        Write-Output $s
+        Write-Output $s | Out-File -FilePath $script:log -Encoding utf8 -Append
+    }
+} #end function Out-Log
+
+
 #endregion functions
 
 #region Script Body
+
+# Enable verbose output
+$VerbosePreference = "Continue"
+
+# Get this script
+$ThisScript = $Script:MyInvocation.MyCommand
+# Get the directory of this script
+$scriptDir = Split-Path $ThisScript.Path -Parent
+# Get the script file
+$scriptFile = Get-Item $ThisScript.Path
+# Get the name of this script
+$scriptName = $scriptFile.Name
+# Get the name of the script less the extension
+$scriptBaseName = $scriptFile.BaseName
+
+# Define folder where log files are written
+$logDir = Join-Path $scriptDir "Logs"
+if ((Test-Path $logDir) -eq $FALSE) {
+    New-Item $logDir -type directory | Out-Null
+}
+
+# The new logfile will be created every day
+$logdate = get-date -format "yyyy-MM-dd"
+$log = Join-Path $logDir ($scriptBaseName +"_" + $logdate + ".log")
+Write-Output "Log file: $log"
+
+Out-Log -Level Info -Message ("{0} script started on {1}" -f $scriptName, $env:COMPUTERNAME)
+
 # Disable Internet Explorer Enhanced Security Configuration
 Disable-InternetExplorerESC
 
 # Install NuGet package provider
+Out-Log -Level Info -Message ("Install NuGet package provider")
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
 # Installing Modules from Azure template 
@@ -103,13 +196,21 @@ $publicIP = Invoke-RestMethod -Headers @{"Metadata"="true"} -URI "http://169.254
 # Demonstrates how to get information from the Azure REST API for this instance.
 $uri = ("https://management.azure.com/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Compute/virtualMachines/{2}?api-version=2017-12-01" -f $compute.subscriptionId, $compute.resourceGroupName, $compute.name)
 $vmInfoRest = (Invoke-WebRequest -Uri $uri  -Method GET -ContentType "application/json" -Headers @{ Authorization ="Bearer $accessToken"}).content | ConvertFrom-JSON
-Write-Output ("Instance ID is: {0}" -f $vmInfoRest.id)
+Out-Log -Level Info -Message ("Instance ID is: {0}" -f $vmInfoRest.id)
 
 # Sign into AzureRM using managed identity
 Add-AzureRmAccount -identity
 $vmInfoPs = Get-AzureRMVM -ResourceGroupName $compute.resourceGroupName -Name $compute.name
 $spID = $vmInfoPs.Identity.PrincipalId
-Write-Output ("The managed identity for Azure resources service principal ID is {0}" -f $spID)
+Out-Log -Level Info -Message ("The managed identity for Azure resources service principal ID is {0}" -f $spID)
 
 Write-Output ("Installing CentreStack build number {0}" -f $Build)
+$adminDBUsername = (Get-AzureKeyVaultSecret -VaultName $VaultName -SecretName 'adminDBUsername').SecretValueText
+$adminDBPassword = (Get-AzureKeyVaultSecret -VaultName $VaultName -SecretName 'adminDBPassword').SecretValueText
+$databaseHost = (Get-AzureKeyVaultSecret -VaultName $VaultName -SecretName 'databaseHost').SecretValueText
+switch ($databaseHost) {
+    "Local" { .\Install-MySQL }
+    "Azure_SQL" { Out-Log -Level Info -Message "Using Azure SQL"}
+    "Azure_MySQL" { Out-Log -Level Info -Message "Using Azure MySQL"}
+}
 #endregion Script Body
---
+++ 101-vm-centrestack/scripts/Install-MySQL.ps1
@@ -0,0 +1,443 @@
+<#
+.Synopsis
+    Installs MySQL Community Edition
+.DESCRIPTION
+    Installs the following MySQL Community Edition products:
+    * Server
+    * Workbench
+    * Connector/NET
+.EXAMPLE
+    .\Install-MySQL.ps1
+.NOTES 
+    Author: Jeff Reed
+    Name: Install-MySQL.ps1
+    Created: 2018-10-23
+    
+    Version History
+    2018-09-24  1.0.0   Initial version
+    2018-09-24  1.0.1   Minor tweaks
+    2018-09-24  1.0.2   Create csmain database and csuser user for principle of least privilege
+#>
+#Requires -Version 5.1
+#Requires -RunAsAdministrator
+[CmdletBinding()]
+Param()
+
+#region functions
+function Install-Product {
+    <#
+    .SYNOPSIS
+        Calls MySQLInstallerConsole.exe to install MySQL products
+    .DESCRIPTION
+        Calls MySQLInstallerConsole.exe to install MySQL products
+    .EXAMPLE
+        Install-Product -Product Server
+    .EXAMPLE
+        Install-Product -Product Workbench
+    .EXAMPLE
+        Install-Product -Product 'Connector/NET'
+    .PARAMETER Product 
+        The product to install
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$true, ValueFromPipeline = $false)]
+        [ValidateSet('Server', 'Workbench', 'Connector/NET')]
+        [string] $Product
+    )
+    
+    switch ($Product) {
+        'Server' { 
+            $version = '5.7.24'
+            $prodPlatform = $platform
+            $fileBaseName = "MySQLInstaller_Server"
+            
+        }
+        'Workbench' {
+            $version = '8.0.13'
+            $prodPlatform = $platform
+            $fileBaseName = "MySQLInstaller_Workbench"
+        }
+        'Connector/NET' {
+            $version = '8.0.13'
+            # Connect/NET is x86 only
+            $prodPlatform = 'x86'
+            $fileBaseName = "MySQLInstaller_ConnectorNet"
+        }
+    }
+    # Write install logs to temp
+    $fileErrLog = Join-Path $env:TEMP ("{0}_StdErr.log" -f $fileBaseName)
+    $fileOutputLog = Join-Path $env:TEMP ("{0}_StdOut.log" -f $fileBaseName)
+    $productString = ("{0};{1};{2}" -f $Product, $version, $prodPlatform)
+
+    $argList = @('community', 'install', $productString, '-silent')
+
+    if ($platform -eq "x64") {
+        # 64 bit
+        $exePath = "C:\Program Files (x86)\MySQL\MySQL Installer for Windows\MySQLInstallerConsole.exe"
+    } else {
+        # 32 bit
+        $exePath = "C:\Program Files\MySQL\MySQL Installer for Windows\MySQLInstallerConsole.exe"
+    }
+    
+    $m = Measure-Command {$proc = Start-Process -FilePath $exePath  -ArgumentList $argList -Wait -RedirectStandardError $fileErrLog -RedirectStandardOutput $fileOutputLog -PassThru }
+    Write-Verbose ("MySQLInstaller completed in: {0:g}" -f $m)
+    Write-Verbose ("MySQLInstaller exit code: {0}" -f $proc.ExitCode)
+    if ($proc.ExitCode -ne 0) {
+        Get-Content $fileErrLog
+        Throw $proc.ExitCode
+    } else {
+        Get-Content $fileOutputLog
+    }
+    
+    # Remove zero byte files
+    $outputFiles = @($fileOutputLog, $fileErrLog)
+    $outputFiles | ForEach-Object {
+        if ((Get-Item $_).Length -eq 0) {
+            # Remove the file if it's zero length
+            Remove-Item $_
+        }
+    }
+} # end function Install-Product
+
+function Install-VCRuntime {
+    <#
+    .SYNOPSIS
+        Downloads and installs the Visual C++ runtime specified by the caller
+    .DESCRIPTION
+        Downloads and installs the Visual C++ runtime specified by the caller
+    .EXAMPLE
+        Install-VCRuntime -URI 'http://download.microsoft.com/download/C/C/2/CC2DF5F8-4454-44B4-802D-5EA68D086676/vcredist_x64.exe' -Name 'Microsoft Visual C++ 2013 Update 5 Redistributable x64'
+    .PARAMETER URI 
+        The URI of the product to download and install
+    .PARAMETER Name
+        The display name that will be output
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$true, ValueFromPipeline = $false)]
+        [string] $URI,
+
+        [Parameter(Mandatory=$true, ValueFromPipeline = $false)]
+        [string] $Name
+
+    )
+    $fileName = $URI.Split('/') | Select-Object -Last 1
+    $outputFile = Join-Path $downloadsFolder $fileName
+
+    # Download and install 
+    Write-Verbose ("Downloading {0} ..." -f $URI)
+    $progressPreference = 'silentlyContinue'
+    try {
+        $m = Measure-Command {Invoke-WebRequest -Uri $URI -OutFile $outputFile}
+    }
+    catch {
+        Write-Warning ("An error occurred while downloading {0}" -f $URI)
+        Throw $_.exception.message
+    }
+    $progressPreference = 'Continue'
+    Write-Verbose ("Completed download in: {0:g}" -f $m)
+    $argList = @('/install', '/passive', '/norestart')
+    $m = Measure-Command {Start-Process -FilePath $outputFile -ArgumentList $argList -Wait}
+    Write-Verbose ("'{0}' installation completed in: {1:g}" -f $Name, $m)
+
+} # end function Install-VCRuntime
+
+function Start-Command {
+    <#
+    .SYNOPSIS
+        Executes a MySQL command to configure the system
+    .DESCRIPTION
+        Executes a MySQL command to configure the system
+    .EXAMPLE
+        Start-Command -Path -ArgList -Action
+    .PARAMETER Path 
+        The path to the executable
+    .PARAMETER ArgList
+        An array of command line arguments
+    .PARAMETER Action
+        The name of the action (used for output log file names)
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$true, ValueFromPipeline = $false)]
+        [string] $Path,
+
+        [Parameter(Mandatory=$true, ValueFromPipeline = $false)]
+        [string[]] $ArgList,
+
+        [Parameter(Mandatory=$true, ValueFromPipeline = $false)]
+        [string] $Action
+    )
+
+    $fileErrLog = Join-Path $env:TEMP ("{0}_StdErr.log" -f $Action)
+    $fileOutputLog = Join-Path $env:TEMP ("{0}_StdOut.log" -f $Action)
+
+    $m = Measure-Command {$proc = Start-Process -FilePath $Path  -ArgumentList $ArgList -Wait -RedirectStandardError $fileErrLog -RedirectStandardOutput $fileOutputLog -PassThru }
+    Write-Verbose ("mysqld install exit code: {0}" -f $proc.ExitCode)
+    if ($proc.ExitCode -ne 0) {
+        Get-Content $fileErrLog
+    } else {
+        Get-Content $fileOutputLog
+    }
+    # Remove zero byte files
+    $outputFiles = @($fileOutputLog, $fileErrLog)
+    $outputFiles | ForEach-Object {
+        if ((Get-Item $_).Length -eq 0) {
+            # Remove the file if it's zero length
+            Remove-Item $_
+        }
+    }
+} #end function Start-Command
+
+#endregion functions
+#region Here-String
+# Set up a Here-String of the text that will be saved in my-template.ini
+$iniText = @"
+# MySQL Server Instance Configuration File
+# ----------------------------------------------------------------------
+# For advice on how to change settings please see
+# http://dev.mysql.com/doc/refman/5.7/en/server-configuration-defaults.html
+
+[client]
+port=3306
+
+[mysql]
+no-beep
+
+[mysqld]
+port=3306
+basedir="C:/Program Files/MySQL/MySQL Server 5.7/"
+datadir=C:/ProgramData/MySQL/MySQL Server 5.7/data
+default-storage-engine=INNODB
+sql-mode="STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
+log-output=FILE
+general-log=0
+general_log_file="%COMPUTERNAME%.log"
+slow-query-log=1
+slow_query_log_file="%COMPUTERNAME%-slow.log"
+long_query_time=10
+log-error="%COMPUTERNAME%.err"
+server-id=1
+lower_case_table_names=1
+secure-file-priv="C:/ProgramData/MySQL/MySQL Server 5.7/Uploads"
+max_connections=151
+table_open_cache=2000
+tmp_table_size=16M
+thread_cache_size=10
+myisam_max_sort_file_size=100G
+myisam_sort_buffer_size=8M
+key_buffer_size=8M
+read_buffer_size=0
+read_rnd_buffer_size=0
+innodb_flush_log_at_trx_commit=1
+innodb_log_buffer_size=1M
+innodb_buffer_pool_size=8M
+innodb_log_file_size=48M
+innodb_thread_concurrency=8
+innodb_autoextend_increment=64
+innodb_buffer_pool_instances=8
+innodb_concurrency_tickets=5000
+innodb_old_blocks_time=1000
+innodb_open_files=300
+innodb_stats_on_metadata=0
+innodb_file_per_table=1
+innodb_checksum_algorithm=0
+back_log=80
+flush_time=0
+join_buffer_size=256K
+max_allowed_packet=4M
+max_connect_errors=100
+open_files_limit=4161
+sort_buffer_size=256K
+table_definition_cache=1400
+binlog_row_event_max_size=8K
+sync_master_info=10000
+sync_relay_log=10000
+sync_relay_log_info=10000
+"@
+#endregion Here-String
+#region Script Body
+#region Setup
+# Start a stopwatch to time the entire script execution
+$swScript = [System.Diagnostics.Stopwatch]::StartNew()
+
+# Force TLSv1.2 else Invoke-WebRequest may throw "The underlying connection was closed: An unexpected error occurred on a send."
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Get the user's Documents folder for output files
+$myDocs = Join-Path $env:UserProfile "Documents"
+
+# This will return '32-bit' or '64-bit'
+$osArchitecture = (Get-WmiObject -Class Win32_OperatingSystem).OSArchitecture
+
+if ($osArchitecture -eq '64-bit') {
+    # Make sure 64-bit PowerShell is running on 64-bit Windows
+    if ([System.IntPtr]::Size -eq 8) {
+        $platform = "x64"
+    }
+    else {
+        Write-Warning "This script is not supported in a 32-bit PowerShell session on 64-bit Windows. Please execute this script within a 64-bit PowerShell session."
+        Exit 1
+    }
+} 
+else {
+    $platform = "x86"
+}
+
+# Specify the location where downloads will be saved
+$downloadsFolder = Join-Path $env:UserProfile "Downloads"
+if (-not (Test-Path $downloadsFolder)) {
+    # Create the folder as necessary
+    New-Item -Path $downloadsFolder -ItemType Directory | Out-Null
+}
+#endregion Setup
+
+#region MySQL Server install
+
+# Download and install Microsoft Visual C++ 2013 Update 5 Redistributable
+if ($platform -eq "x64") {
+    # 64 bit
+    $htParams = @{
+        URI = 'http://download.microsoft.com/download/0/5/6/056DCDA9-D667-4E27-8001-8A0C6971D6B1/vcredist_x64.exe'
+        Name = 'Microsoft Visual C++ 2013 Update 5 Redistributable x64'
+    }
+} else {
+    # 32 bit
+    $htParams = @{
+        URI = 'http://download.microsoft.com/download/0/5/6/056DCDA9-D667-4E27-8001-8A0C6971D6B1/vcredist_x86.exe'
+        Name = 'Microsoft Visual C++ 2013 Update 5 Redistributable x86'
+    }
+}
+Install-VCRuntime @htParams
+
+# Download MySQL installer package
+$uriMySQL = 'https://cdn.mysql.com//Downloads/MySQLInstaller/mysql-installer-web-community-5.7.24.0.msi'
+
+$fileName = $uriMySQL.Split('/') | Select-Object -Last 1
+$outputFile = Join-Path $downloadsFolder $fileName
+Write-Verbose ("Downloading {0} ..." -f $uriMySQL)
+$progressPreference = 'silentlyContinue'
+try {
+    $m = Measure-Command {Invoke-WebRequest -Uri $uriMySQL -OutFile $outputFile}
+}
+catch {
+    Write-Warning ("An error occurred while downloading {0}" -f $uriCSUpgrade)
+    Throw $_.exception.message
+}
+$progressPreference = 'Continue'
+Write-Verbose ("Completed download in: {0:g}" -f $m)
+$argList = @('/i', $outputFile, '/passive')
+$m = Measure-Command {Start-Process -FilePath msiexec.exe -ArgumentList $argList -Wait}
+Write-Verbose ("MySQLInstaller installation completed in: {0:g}" -f $m)
+
+Install-Product -Product "Server"
+
+# Create default Data directory
+$dataDir = 'C:/ProgramData/MySQL/MySQL Server 5.7/data'
+if (-not (Test-Path $dataDir) ) {
+    New-Item -Path $dataDir -ItemType Directory | Out-Null
+}
+
+# Create Uploads directory
+$uploadsDir = 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads'
+if (-not (Test-Path $uploadsDir) ) {
+    New-Item -Path $uploadsDir -ItemType Directory | Out-Null
+}
+
+# Replace %COMPUTERNAME% string in Here-String with actual computername
+$iniText = $iniText.Replace('%COMPUTERNAME%', $env:ComputerName)
+# Write a my.ini file from the Here-String text
+$defaultsFile = 'C:/ProgramData/MySQL/MySQL Server 5.7/my.ini'
+[System.IO.File]::WriteAllText($defaultsFile, $iniText )
+
+# Generate an 8 character random password and save it in the mysql-init.txt file in the user's Documents directory
+[Reflection.Assembly]::LoadWithPartialName("System.Web") | Out-Null
+$rootPassword = [System.Web.Security.Membership]::GeneratePassword(8,0)
+$initFile = Join-Path $myDocs "mysql-init.txt"
+$sqlCMD = "ALTER USER 'root'@'localhost' IDENTIFIED BY '$rootPassword';"
+$sqlCMD  | Out-File -FilePath $initFile -Encoding utf8
+
+# Initialize MySQL with --initialize-insecure
+$htParams = @{
+    Path = "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqld.exe"
+    ArgList = @('--initialize-insecure')
+    Action = "MySQLInitialize"
+}
+Start-Command @htParams
+
+# Install MySQL as a Windows service
+$htParams = @{
+    Path = "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqld.exe"
+    ArgList = @('--install')
+    Action = "MySQLInstallService"
+}
+Start-Command @htParams
+
+# Start the MySQL Service
+Start-Service -Name MySQL -Verbose
+
+# Change the root password
+$argList = @()
+$argList += "-uroot"
+$argList += "--execute=`"$sqlCMD`""
+$htParams = @{
+    Path = "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe"
+    ArgList = $argList
+    Action = "MySQLChangeRootPassword"
+}
+Start-Command @htParams
+#endregion MySQL Server install
+
+#region Install MySQL Workbench 8.0
+
+# Download and install Microsoft Visual C++ 2015 Redistributable
+if ($platform -eq "x64") {
+    # 64 bit
+    $htParams = @{
+        URI = 'https://download.microsoft.com/download/6/D/F/6DF3FF94-F7F9-4F0B-838C-A328D1A7D0EE/vc_redist.x64.exe'
+        Name = 'Microsoft Visual C++ 2015 Redistributable Update 3 x64'
+    }
+} else {
+    # 32 bit
+    $htParams = @{
+        URI = 'https://download.microsoft.com/download/6/D/F/6DF3FF94-F7F9-4F0B-838C-A328D1A7D0EE/vc_redist.x86.exe'
+        Name = 'Microsoft Visual C++ 2015 Redistributable Update 3 x86'
+    }
+}
+Install-VCRuntime @htParams
+
+Install-Product -Product "Workbench"
+#endregion Install MySQL Workbench 8.0
+
+#region Install Connector/NET
+Install-Product -Product 'Connector/NET'
+#endregion Install Connector/NET
+
+#region Create Database and User
+# Create 'csmain' database and 'csuser' user. Grant csuser all privileges to csmain
+$csuserPassword = [System.Web.Security.Membership]::GeneratePassword(8,0)
+$sqlCMD = @"
+CREATE DATABASE IF NOT EXISTS csmain;
+CREATE USER 'csuser'@'localhost' IDENTIFIED BY '$csuserPassword';
+GRANT ALL ON csmain.* TO 'csuser'@'localhost';
+"@
+$createFile = Join-Path $myDocs "mysql-create.txt"
+$sqlCMD  | Out-File -FilePath $createFile -Encoding utf8
+
+$argList = @()
+$argList += "-uroot"
+$argList += "-p$rootPassword"
+$argList += "--execute=`"source $createFile`""
+$htParams = @{
+    Path = "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe"
+    ArgList = $argList
+    Action = "MySQLCreateDBandUser"
+}
+Start-Command @htParams
+#endregion Create Database and User
+
+# Stop the stopwatch
+$swScript.Stop()
+Write-Output ("`nScript completed in: {0:g}" -f $swScript.Elapsed)
+#endregion Script Body
